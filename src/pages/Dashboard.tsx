52d51629a96fbe158f592b93ca8c33abb3b9c9c7
Hyowon Ahn <hyowon.ahn@student.mancehster.ac.uk>
2025-09-14 19:17:03 +0900

    delete unused values

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { useLocation, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import DeckCard from "../components/DeckCard";
 import SectionHeader from "../components/SectionHeader";
 
